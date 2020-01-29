8d31c6b5f97015b2470dacc5dda83e9a88336b0c
bitwkit <bitwkit@gmail.com>
2020-01-29 17:22:27 +0300

    test: added test class with mock function

--- tests/resource-pools.test.js
+++ tests/resource-pools.test.js
@@ -1,19 +1,51 @@
 
-const {ResourcePool} = require('../src/resource-pools.js');
+const {ResourcePool, isSym, readyEventSym, errorEventSym, closeMethodSym} = require('../src/resource-pools.js');
+const EventEmitter = require('events');
+
+
+// Mock class
+
+const mockFn = jest.fn( cb => cb() );
+
+class TestResource extends EventEmitter {
+    constructor(cb, ...args) {
+        super(...args);
+        setImmediate(() => mockFn(() => cb.call(this)));
+    };
+};
+
+const testConfig = {
+    constructor: TestResource,
+    arguments: [function() {this.emit(readyEventSym)}],
+    maxCount: 1
+};
+
 
 // complete tests
 
+
 // incomplete tests
 
-test('initial start', () => {
-    expect(true).toBe(false);
+test('on new request and no resources available, pool size below max: calls a resource constructor with args from config', () => {
+    const pool = new ResourcePool(testConfig);
+    return pool.allocate().then( () => {
+        expect(mockFn).toHaveBeenCalledTimes(1);
+        //expect(mockFn.calls[0]).toEqual([testArgs]);
+    });
 });
 
-test('on new request and no resources available, pool size below max: calls a resource constructor', () => {
+
+// tests to make
+/*
+test('passes arguments to the resource constructor when calling it', () => {
     expect(true).toBe(false);
 });
 
 test('on new request and no resources available, pool size is at max: puts the request to a queue', () => {
+    expect(true).toBe(false);
+});
+
+test('on new request and no resources available, pool size is at max: does not create a new resource', () => {
     expect(true).toBe(false);
 });
 
@@ -32,3 +64,4 @@ test('on allocated resource fail: calls resource close method', () => {
 test('on idle resource fail: deletes it from idle queue and calls resource close method', () => {
     expect(true).toBe(false);
 });
+*/
